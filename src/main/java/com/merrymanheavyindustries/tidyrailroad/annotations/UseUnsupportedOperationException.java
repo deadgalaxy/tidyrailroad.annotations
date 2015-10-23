9e71402decd625378814847dd3fbc3d61eb7de00
Emory Merryman <emory.merryman@gmail.com>
2015-10-22 23:35:40 -0400

    added copyright and licening

--- src/main/java/com/merrymanheavyindustries/tidyrailroad/annotations/UseUnsupportedOperationException.java
+++ src/main/java/com/merrymanheavyindustries/tidyrailroad/annotations/UseUnsupportedOperationException.java
@@ -1,9 +1,38 @@
+/*
+    Copyright (C) 2015
+    Emory Merryman
+    emory.merryman@gmail.com
+
+    This file is part of tidyrailroad.
+
+    tidyrailroad is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    tidyrailroad is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with tidyrailroad.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.merrymanheavyindustries.tidyrailroad;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 /**
  * Use an @see java.lang.UnsupportedOperationException to satisfy
  * the dependency.
  **/
+@Documented
+@Retention(SOURCE)
+@Target(METHOD)
 public @interface UseUnsupportedOperationException {
     /**
      * A format string for the error message.
